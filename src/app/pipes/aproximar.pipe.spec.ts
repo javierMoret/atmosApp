639d8ba89d56ae6320f175b830bebc16fb370589
javierMoret <javiermoret003@gmail.com>
2023-05-29 17:31:11 +0200

    Aa

--- src/app/pipes/aproximar.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AproximarPipe } from './aproximar.pipe';
-
-describe('AproximarPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AproximarPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/pipes/primera-mayuscula.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PrimeraMayusculaPipe } from './primera-mayuscula.pipe';
-
-describe('PrimeraMayusculaPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PrimeraMayusculaPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
